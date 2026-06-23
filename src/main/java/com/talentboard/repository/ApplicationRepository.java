7a92d88bcd8a1b5188708ffd83ab941bcf98f4b9
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-23 09:59:59 -0500

    feat(ApplicationRepository): add methods to get applications throught its candidates and details

---
+++ src/main/java/com/talentboard/repository/ApplicationRepository.java
@@ -0,0 +1,19 @@
+package com.talentboard.repository;
+
+import com.talentboard.entities.ApplicationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ApplicationRepository extends JpaRepository<ApplicationEntity, UUID> {
+
+    @Query("SELECT a FROM Application a JOIN FETCH a.vacancy WHERE a.candidate.id = :candidateId")
+    List<ApplicationEntity> findByCandidateId(@Param("candidateId") Long candidateId);
+
+    @Query("SELECT a FROM Application a JOIN FETCH a.candidate JOIN FETCH a.vacancy")
+    List<ApplicationEntity> findAllWithDetails();
+
+}
