ace206766ec02c6beba6919f2bc77909c64efc75
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-23 07:23:54 -0500

    feat(User): add POJO for user model

--- src/main/java/com/talentboard/models/User.java
+++ src/main/java/com/talentboard/models/User.java
@@ -1,4 +1,29 @@
 package com.talentboard.models;
 
+import com.talentboard.enums.Roles;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
+
+    private UUID id;
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    private Roles role;
+
 }
